76c8c1e233db9bc5349c1ff8390d770f4fafa404
totti-love <totti.prtama@gmail.com>
2024-10-25 14:14:31 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+
+
 })->middleware('auth:sanctum');
 
 Route::get('fakultas', [FakultasController::class, 'index']);
+
+
