712d8918e182938fca92fd95d793d444255172ab
totti-love <totti.prtama@gmail.com>
2024-11-08 11:53:36 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\FakultasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
