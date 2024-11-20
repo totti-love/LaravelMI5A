aefcbf0f625e528d2e0f0c4871eb7a72fa19cce4
ferysetiawan <totti.prtama@gmail.com>
2024-11-20 14:45:30 +0700

    put api

--- app/Http/Controllers/FakultasController.php
+++ app/Http/Controllers/FakultasController.php
@@ -144,4 +144,27 @@ class FakultasController extends Controller
             return response()->json($response, 400); //400 Bad Request
         }
     }
+
+    public function updateFakultas(Request $request,  $id)
+    {
+        $fakultas = Fakultas::find($id);
+
+        $input = $request->validate([
+            "nama" => "required",
+            "dekan" => "required",
+            "singkatan" => "required"
+        ]);
+        //update data
+        $hasil = $fakultas->update($input);
+        //redirect beserta pesan sukses
+        if($hasil){ //jika data berhasil disimpan
+            $response['success'] = true;
+            $response['message'] = "Fakultas berhasil diubah";
+            return response()->json($response, 200); //201 utk created 
+        }else {
+            $response['success'] = false;
+            $response['message'] = "Fakultas gagal diubah";
+            return response()->json($response, 400); //400 Bad Request
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('fakultas', [FakultasController::class, 'getFakultas'])->middleware(['auth:sanctum', 'ability:read']);
 Route::post('fakultas', [FakultasController::class, 'storeFakultas'])->middleware(['auth:sanctum', 'ability:create']);
-Route::delete('fakultas/{id}', [FakultasController::class, 'destroyFakultas']);
+Route::delete('fakultas/{id}', [FakultasController::class, 'destroyFakultas'])->middleware(['auth:sanctum', 'ability:delete']);;
+Route::put('fakultas/{id}', [FakultasController::class, 'updateFakultas'])->middleware(['auth:sanctum', 'ability:update']);
 
 Route::get('mahasiswa', [MahasiswaController::class, 'getMahasiswa']);
 Route::get('mahasiswa', [MahasiswaController::class, 'storeMahasiswa']);
